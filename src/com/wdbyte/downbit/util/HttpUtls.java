921d5141c00524498f255e0eb852b37ea45df18b
niumoo <niumoo@126.com>
2020-08-03 14:35:32 +0800

    增加方法注释

--- src/com/wdbyte/downbit/util/HttpUtls.java
+++ src/com/wdbyte/downbit/util/HttpUtls.java
@@ -15,13 +15,29 @@ import java.util.Map;
  */
 public class HttpUtls {
 
+    /**
+     * 获取 HTTP 链接
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
     public static HttpURLConnection getHttpUrlConnection(String url) throws IOException {
         URL httpUrl = new URL(url);
-        HttpURLConnection httpConnection = (HttpURLConnection)httpUrl.openConnection();
+        HttpURLConnection httpConnection = (HttpURLConnection) httpUrl.openConnection();
         httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36");
         return httpConnection;
     }
 
+    /**
+     * 获取 HTTP 链接
+     *
+     * @param url
+     * @param start
+     * @param end
+     * @return
+     * @throws IOException
+     */
     public static HttpURLConnection getHttpUrlConnection(String url, long start, Long end) throws IOException {
         HttpURLConnection httpUrlConnection = getHttpUrlConnection(url);
         if (end != null) {
@@ -32,6 +48,13 @@ public class HttpUtls {
         return httpUrlConnection;
     }
 
+    /**
+     * 获取网络文件大小 bytes
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
     public static long getHttpFileContentLength(String url) throws IOException {
         HttpURLConnection httpUrlConnection = getHttpUrlConnection(url);
         int contentLength = httpUrlConnection.getContentLength();
@@ -39,13 +62,27 @@ public class HttpUtls {
         return contentLength;
     }
 
+    /**
+     * 获取网络文件 Etag
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
     public static String getHttpFileEtag(String url) throws IOException {
         HttpURLConnection httpUrlConnection = getHttpUrlConnection(url);
         Map<String, List<String>> headerFields = httpUrlConnection.getHeaderFields();
         List<String> eTagList = headerFields.get("ETag");
+        httpUrlConnection.disconnect();
         return eTagList.get(0);
     }
 
+    /**
+     * 获取网络文件名
+     *
+     * @param url
+     * @return
+     */
     public static String getHttpFileName(String url) {
         int indexOf = url.lastIndexOf("/");
         return url.substring(indexOf + 1);
